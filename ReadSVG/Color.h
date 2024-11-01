234b745e6138c9b00041f2c463e22a8af5d0ca57
ltp5614 <lethanhphong2005@gmail.com>
2024-11-01 17:57:32 +0700

    Add project files.

---
+++ ReadSVG/Color.h
@@ -0,0 +1,21 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+#include "SVGElements.h"
+
+class Color {
+   private:
+       int red, blue, green;
+       float alpha; // Transparency (default: 255)
+
+   public:
+       Color(int red, int blue, int green, float alpha = 1.0);
+       Color parseColor(const std::string& colorStr);
+       
+       int getRed();
+       int getBlue();
+       int getGreen();
+       float getAlpha();
+};
+
+#endif
